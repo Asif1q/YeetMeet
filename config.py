5e8fed07116481722bb957d8b3b87973025abfa8
Asif1q <54772037+Asif1q@users.noreply.github.com>
2021-04-13 17:28:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,13 @@
 import os
 class Config(object):
-    BOT_TOKEN = os.environ.get('BOT_TOKEN')
-    GUSERNAME = os.environ.get('GUSER_NAME')
-    GPASSWORD = os.environ.get('GPASSWORD')
+    
     SCHEDULE = os.environ.get('SCHEDULE', True)
-    USERID = os.environ.get('USERID')
+    USERID = os.environ.get('USERID', 537370867)
 # If you're not familiar with how to set Environment Variables you can do like this instead
 # of  setting Environment Variables
 
-# BOT_TOKEN = os.environ.get('BOT_TOKEN', '1065989390:AAHzDBaNFujgq6f-sUPyqF0ZBZrX8mKlY9g')
-# GUSERNAME = os.environ.get('GUSER_NAME', 'b17ee092@mace.ac.in ')
-# GPASSWORD = os.environ.get('GPASSWORD', 'ma171@eee')
+    BOT_TOKEN = os.environ.get('BOT_TOKEN', '1065989390:AAHzDBaNFujgq6f-sUPyqF0ZBZrX8mKlY9g')
+    GUSERNAME = os.environ.get('GUSER_NAME', 'b17ee092@mace.ac.in')
+    GPASSWORD = os.environ.get('GPASSWORD', 'ma171@eee')
 
 #NOTE: Putting sensitive information in files is considered unsafe. Try to use ENVIRONMENT VARIABLES 
